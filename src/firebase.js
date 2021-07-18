08ce28110b3696f813adccfd0112919f8b436dae
x00145570 <x00145570@gmail.com>
2021-07-18 13:23:19 +0100

    Inital commit with firebase.js configuration file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgWOpRAH_r-YTI8d-cKZnrEtCEg4RnWCQ",
+    authDomain: "messagingapp-68726.firebaseapp.com",
+    projectId: "messagingapp-68726",
+    storageBucket: "messagingapp-68726.appspot.com",
+    messagingSenderId: "358234541802",
+    appId: "1:358234541802:web:09bbb3cedf8e1230295193",
+    measurementId: "G-J0Q4F24MP2"
+  };
